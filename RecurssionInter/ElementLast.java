e0b9a866589fb35a667f3163a3fcffde2e1954f2
Bharath-6 <67066997+Bharath-6@users.noreply.github.com>
2023-08-21 10:11:14 +0530

    recursion

---
+++ RecurssionInter/ElementLast.java
@@ -0,0 +1,27 @@
+//question: move all 'x' to the end of the string
+
+package RecurssionInter;
+
+public class ElementLast {
+    public static void moveAll(String str, int idx, int count, String newString){
+        if(idx==str.length()){
+            for(int i=0;i<count;i++){
+                newString+='x';
+            }
+            System.out.println(newString);
+            return;
+        }
+        char currChar = str.charAt(idx);
+        if(currChar=='x'){
+            count++;
+            moveAll(str, idx+1, count, newString);
+        }else{
+            newString +=currChar;
+            moveAll(str, idx+1, count, newString);
+        }
+    }
+    public static void main(String args[]){
+        String str="axbcxxd";
+        moveAll(str, 0, 0, "");
+    }
+}
---
+++ RecurssionInter/SubsequnceOfString.java
@@ -0,0 +1,20 @@
+package RecurssionInter;
+
+public class SubsequnceOfString {
+    public static void subsequences(String str, int idx, String newString){
+        if(idx==str.length()){
+             System.err.println(newString);
+        return;
+        }
+    
+        char currChar=str.charAt(idx);
+        //to be 
+        subsequences(str, idx+1, newString+currChar);
+        //or not to be
+        subsequences(str, idx+1, newString);
+    }
+    public static void main(String args[]){
+        String str="abc";
+        subsequences(str, 0, "");
+    }
+}
