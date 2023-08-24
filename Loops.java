e169f303fc2005ced5cabf85270eb88b5972db9c
Bharath-6 <67066997+Bharath-6@users.noreply.github.com>
2023-08-24 11:32:55 +0530

    loops

---
+++ Loops.java
@@ -0,0 +1,25 @@
+public class Loops {
+    public static void main(String args[]){
+        // for(int counter=0; counter<5;counter++){
+        //     System.out.println("hello world");
+    //}
+        //printing 0 to 10
+        // for(int i=0;i<11;i++){
+        //     System.out.println(i);
+        // }
+        // int i=0;
+        // while(i<11){
+        //     System.out.println(i);
+        //     i++;
+        // }
+        int i=0;
+        do{
+            System.out.println(i);
+            i=i+1;
+        }
+            while(i<11);
+        
+    }
+
+    }
+
---
+++ SumOfNatural.java
@@ -0,0 +1,11 @@
+public class SumOfNatural {
+    public static void main(String args[]){
+        int sum=0;
+        int n=4;
+        for(int i=0;i<=n;i++){
+            sum=sum+i;
+            
+        }
+         System.out.println(sum);
+    }
+}
